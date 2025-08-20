d113712a2cb96cb355e66c11fff2f5a238d482f7
Jenn <96408342+jpcreativeworks@users.noreply.github.com>
2025-08-20 12:13:56 -0700

    I added the GitHub repo URL

--- src/App.js
+++ src/App.js
@@ -6,6 +6,11 @@ function App() {
   return (
     <div className="App">
       React Weather App 
+
+      <footer>
+        <p>This project was created by <a href="https://github.com/jpcreativeworks" target="_blank" rel="noreferrer">Jenn Bencriscutto</a></p>
+        <a href="https://github.com/jpcreativeworks/another-react-weather-application.git" target="_blank" rel="noreferrer">GitHub</a>
+      </footer>
     </div>
   );
 }
