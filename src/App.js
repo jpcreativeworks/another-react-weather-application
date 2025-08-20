ce662b3453db5bb125225705e2d0a866c2398f78
Jenn <96408342+jpcreativeworks@users.noreply.github.com>
2025-08-20 12:28:45 -0700

    added bootstrap

--- src/App.js
+++ src/App.js
@@ -5,13 +5,15 @@ import './App.css';
 function App() {
   return (
     <div className="App">
-      <h1>
-      React Weather App 
-      </h1>
-      <footer>
-        <p>This project was created by <a href="https://github.com/jpcreativeworks" target="_blank" rel="noreferrer">Jenn Bencriscutto</a></p>
-        <a href="https://github.com/jpcreativeworks/another-react-weather-application.git" target="_blank" rel="noreferrer">GitHub</a>
-      </footer>
+      <div className="container">
+        <h1>
+        React Weather App 
+        </h1>
+        <footer>
+          <p>This project was created by <a href="https://github.com/jpcreativeworks" target="_blank" rel="noreferrer">Jenn Bencriscutto</a></p>
+          <a href="https://github.com/jpcreativeworks/another-react-weather-application.git" target="_blank" rel="noreferrer">GitHub</a>
+        </footer>
+      </div>
     </div>
   );
 }
