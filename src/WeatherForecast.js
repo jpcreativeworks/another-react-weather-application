96841226bf5c9d64c2fab4ed0febc70999629956
Jenn <96408342+jpcreativeworks@users.noreply.github.com>
2025-08-21 03:16:43 -0700

    fixed return request on loop for forecast

--- src/WeatherForecast.js
+++ src/WeatherForecast.js
@@ -21,15 +21,15 @@ export default function WeatherForecast(props) {
     return(
       <div className="WeatherForecast">
         <div className="row">
-          {forecastData.map(function(dailyForecast, index) {
+          {forecastData.map(function (dailyForecast, index) {
             if (index < 5) {
               return (
                 <div className="col" key={index}>
                   <WeatherForecastDay data={dailyForecast} />
                 </div>
-              )
-            }
-          })}
+              );
+            } else {return(null)}
+          })} 
           
         </div>
       </div>
