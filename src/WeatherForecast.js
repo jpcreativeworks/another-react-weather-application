3767b5d22ae595feae90357a7292b848fae5e7f4
Jenn <96408342+jpcreativeworks@users.noreply.github.com>
2025-08-21 01:57:06 -0700

    used api to connect city to forecast response

--- src/WeatherForecast.js
+++ src/WeatherForecast.js
@@ -1,8 +1,18 @@
 import React from "react"
 import WeatherIcon from "./WeatherIcon";
 import "./WeatherForecast.css";
+import axios from "axios"
 
-export default function WeatherForecast() {
+export default function WeatherForecast(props) {
+  function handleResponse(response) {
+    console.log(response.data)
+  }
+  console.log(props);
+
+  const key = "7fd5430a29oa8949b4d239de06t9a3d4"
+  const city = props.city
+  const forecastUrl = `https://api.shecodes.io/weather/v1/forecast?query=${city}&key=${key}&untis={metric}`
+  axios.get(forecastUrl).then(handleResponse);
   return(
     <div className="WeatherForecast">
       <div className="row">
